ecb93080a90ef5cc376dc67752c9b771f07bdc31
Adam Chmura <adi5166@op.pl>
2020-08-27 22:02:58 +0200

    Start

---
+++ app/src/main/java/com/adam51/przypominacz_leki/fragment/HomeFragment.java
@@ -0,0 +1,24 @@
+package com.adam51.przypominacz_leki.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.adam51.przypominacz_leki.R;
+
+public class HomeFragment extends Fragment {
+
+  public HomeFragment() {
+    // Required empty public constructor
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_home, container, false);
+  }
+}
