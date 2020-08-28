6b7d42c99b5202554a948ebc8ee3f2051a8cdc09
Adam Chmura <adi5166@op.pl>
2020-08-28 22:33:03 +0200

    Użycie ViewBinding

--- app/src/main/java/com/adam51/przypominacz_leki/MainActivity.java
+++ app/src/main/java/com/adam51/przypominacz_leki/MainActivity.java
@@ -6,17 +6,27 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.adam51.przypominacz_leki.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+  private ActivityMainBinding binding;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
 
-    BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+    //View view = binding.getRoot();
+    //setContentView(view);
+    //setContentView(R.layout.activity_main);
+    //BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+    BottomNavigationView bottomNavigationView = binding.bottomNavigationView;
 
     NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
     NavigationUI.setupWithNavController(bottomNavigationView, navController);
